3d9df66d58195592335f0e667e055a1c9dda8aa5
tylerjusfly <tylerjusfly1@gmail.com>
2024-10-04 19:52:50 +0100

    setting up zustand and top bar

---
+++ src/components/topbar/TopBar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TopBar = () => {
+  return (
+    <div>
+      <h2 id="tour1-step2">Hello</h2>
+      <h2>Hi</h2>
+    </div>
+  );
+};
+
+export default TopBar;
