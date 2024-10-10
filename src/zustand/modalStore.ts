226f9f3af97b0fbd18a6e80ad242649dfa212572
tylerjusfly <tylerjusfly1@gmail.com>
2024-10-10 17:58:49 +0100

    modal store refactor

--- src/zustand/modalStore.ts
+++ src/zustand/modalStore.ts
@@ -2,31 +2,21 @@ import { create } from "zustand";
 import createSelectors from "@/zustand/config/createSelector";
 
 interface ModalState {
-  //   isJoinSpaceModalOpen: boolean;
-  //   isCreateSpaceModalOpen: boolean;
-  //   isCreateCategoryModalOpen: boolean;
   isAvatarModalOpen: boolean;
   isForgetPasswordModalOpen: boolean;
   isNotLoggedInModalOpen: boolean;
-  //   space: Spaces | null;
 }
 
 interface ModalStoreState extends ModalState {
   //   openModal: (kind: keyof Omit<ModalState, "space">) => void;
   openModal: (kind: keyof ModalState) => void;
   closeModal: () => void;
-  // setSpace: (space: Spaces) => void;
-  // clearSpace: () => void;
 }
 
 const initialState = {
-  // isJoinSpaceModalOpen: false,
-  // isCreateSpaceModalOpen: false,
-  // isCreateCategoryModalOpen: false,
   isAvatarModalOpen: false,
   isForgetPasswordModalOpen: false,
   isNotLoggedInModalOpen: false,
-  // space: null,
 };
 
 const modalStore = create<ModalStoreState>()((set) => ({
